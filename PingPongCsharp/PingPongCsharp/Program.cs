7cc1e16c74ef747d6796cc40a7c120bae94396ae
kevin <giroux.kevin.fr@gmail.com>
2015-03-24 15:33:19 +0100

    Commit

--- PingPongCsharp/PingPongCsharp/Program.cs
+++ PingPongCsharp/PingPongCsharp/Program.cs
@@ -14,6 +14,7 @@ namespace PingPongCsharp
         [STAThread]
         static void Main()
         {
+            System.Console.WriteLine("HelloWorld");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
