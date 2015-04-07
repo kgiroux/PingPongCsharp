754f5150088d9029ab12da7f38f77263430e00e1
LirycMoaners <cyril.lefebvre.27@gmail.com>
2015-04-07 17:05:58 +0200

    Partie v4.1

---
+++ PingPongCsharp/PingPongCsharp/Balle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongCsharp
+{
+    class Balle
+    {
+        private double angle;
+        private int vitesse;
+
+        public Balle()
+        {
+            vitesse = 0;
+            angle = 0;
+        }
+        public void Lance()
+        {
+            vitesse = 20;
+
+        }
+    }
+}
