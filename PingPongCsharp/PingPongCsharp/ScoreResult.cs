6772b92ec73960d18e88b8da93d46a2590a9bb0e
kevin <giroux.kevin.fr@gmail.com>
2015-05-19 17:04:21 +0200

    Hello world

---
+++ PingPongCsharp/PingPongCsharp/ScoreResult.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PingPongCsharp;
+using System.Data.Entity.Core.Objects;
+
+namespace PingPongCsharp
+{
+    public partial class ScoreResult : Form
+    {
+        ScoreResultEntities pongResult = new ScoreResultEntities();
+
+        public ScoreResult()
+        {
+            InitializeComponent();
+            var query = from score in pongResult.Scores select new { score.NomServeur, score.ScoreServeur, score.ScoreClient, score.NomClient };
+            ResultDataGrid.DataSource = query.ToList();
+            ResultDataGrid.Columns[0].HeaderText = "Nom Joueur 1";
+            ResultDataGrid.Columns[1].HeaderText = "Score du Joueur 1";
+            ResultDataGrid.Columns[2].HeaderText = "Score du Joueur 2";
+            ResultDataGrid.Columns[3].HeaderText = "Nom du Joueur 2";
+            
+            ResultDataGrid.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            ResultDataGrid.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            ResultDataGrid.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            ResultDataGrid.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+           
+            //ResultDataGrid.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells); 
+                
+        }
+    }
+}
---
+++ PingPongCsharp/PingPongCsharp/Welcome.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PingPongCsharp
+{
+    partial class Welcome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcome));
+            this.Start = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Scores = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(50, 211);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(99, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(352, 193);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Scores
+            // 
+            this.Scores.Location = new System.Drawing.Point(238, 211);
+            this.Scores.Name = "Scores";
+            this.Scores.Size = new System.Drawing.Size(99, 23);
+            this.Scores.TabIndex = 2;
+            this.Scores.Text = "Scores";
+            this.Scores.UseVisualStyleBackColor = true;
+            this.Scores.Click += new System.EventHandler(this.Scores_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(7, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Create by Kévin Giroux and Cyril Lefebvre";
+            // 
+            // Welcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 307);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Scores);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Start);
+            this.Name = "Welcome";
+            this.Text = "PingPongC#";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Scores;
+        private System.Windows.Forms.Label label1;
+    }
+}
