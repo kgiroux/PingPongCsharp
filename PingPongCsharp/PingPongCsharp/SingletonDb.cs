8f93a9d2eedd8c47f1ae1ac961bb27e1a8e5ceaa
kevin <giroux.kevin.fr@gmail.com>
2015-05-22 15:32:05 +0200

    sdfghj

---
+++ PingPongCsharp/PingPongCsharp/SingletonDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongCsharp
+{
+    public sealed class SingletonDb
+    {
+        private static readonly object padlock = new object();
+        private static ScoreResultEntities pongResult = null;
+
+        public SingletonDb()
+        {
+        }
+
+        public static ScoreResultEntities Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (pongResult == null)
+                    {
+                        pongResult = new ScoreResultEntities();
+                    }
+                    return pongResult;
+                }
+            }
+        }
+    }
+}
