512abf4b5f2dfc1fa2f63198ad2710016537a955
LirycMoaners <cyril.lefebvre.27@gmail.com>
2015-05-25 10:19:06 +0200

    Réparation

---
+++ PingPongCsharp/PingPongCsharp/SingletonDb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPongCsharp
+{
+    public sealed class SingletonDb
+    {
+        private static readonly object padlock = new object();
+        private static ScoreResultEntities pongResult = null;
+        /// <summary>
+        /// Constructeur vide pour un singleton
+        /// </summary>
+        public SingletonDb()
+        {
+        }
+        /// <summary>
+        /// Permet de crée une unique instance de ScoreResultEntities;
+        /// </summary>
+        public static ScoreResultEntities Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (pongResult == null)
+                    {
+                        pongResult = new ScoreResultEntities();
+                    }
+                    return pongResult;
+                }
+            }
+        }
+    }
+}
